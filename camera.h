7b5d4224eb42247e66cd9a4f35a8ce8853a59638
Adrian Garza <adrianhgarza@gmail.com>
2021-03-26 15:12:21 -0700

    camera field name update

--- camera.h
+++ camera.h
@@ -8,29 +8,29 @@ class camera {
     private:
         point3 origin;
         point3 bottom_left_corner;
-        vec3 horizontal;
-        vec3 vertical;
+        vec3 horizontal_size;
+        vec3 vertical_size;
         float aspect_ratio;
 
     public:
         camera(float aspect) {
             aspect_ratio = aspect;
-            
+
             float viewport_height = 2.0;
             float viewport_width = aspect_ratio * viewport_height;
             float focal_length = 1.0;
             
             origin = point3(0, 0, 0);
-            horizontal = vec3(viewport_width, 0, 0);
-            vertical = vec3(0, viewport_height, 0);
+            horizontal_size = vec3(viewport_width, 0, 0);
+            vertical_size = vec3(0, viewport_height, 0);
             bottom_left_corner = origin 
-                - (horizontal/2)            // x
-                - (vertical/2)              // y
+                - (horizontal_size/2)            // x
+                - (vertical_size/2)              // y
                 - vec3(0, 0, focal_length); // z
         }
 
         ray get_ray(float u, float v) const {
-            return ray(origin, bottom_left_corner + u*horizontal + v*vertical - origin);
+            return ray(origin, bottom_left_corner + u*horizontal_size + v*vertical_size - origin);
         }
 };
 
